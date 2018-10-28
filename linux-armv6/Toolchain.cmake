9b81318e8684bddd871820a88af66405799798ab
Connor Christie <christieck@msoe.edu>
2018-10-28 18:11:26 -0500

    Fix directories for crosstool-ng

--- linux-armv6/Toolchain.cmake
+++ linux-armv6/Toolchain.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-set(cross_triple "armv6-unknown-linux-gnueabihf")
+set(cross_triple "arm-linux-gnueabihf")
 set(cross_root /usr/xcc/${cross_triple})
 
 set(CMAKE_C_COMPILER $ENV{CC})
