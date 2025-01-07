f5b3739d8d6f6f083c1777edf15c15ca736b5d03
Matt McCormick <matt@mmmccormick.com>
2025-01-07 10:59:10 -0500

    web-wasi-threads: use target wasm32-wasi-threads in sysroot wrapper
    And add -pthread flag.

--- web-wasi-threads/Toolchain.cmake
+++ web-wasi-threads/Toolchain.cmake
@@ -4,7 +4,7 @@ include($ENV{WASI_SDK_PATH}/share/cmake/wasi-sdk-pthread.cmake)
 set(CMAKE_FIND_ROOT_PATH $ENV{CROSS_ROOT})
 set(CMAKE_SYSROOT $ENV{WASI_SYSROOT})
 
-set(CMAKE_C_COMPILER /usr/local/bin/clang-wasi-sysroot.sh)
-set(CMAKE_CXX_COMPILER /usr/local/bin/clang++-wasi-sysroot.sh)
+set(CMAKE_C_COMPILER /usr/local/bin/clang-wasi-threads-sysroot.sh)
+set(CMAKE_CXX_COMPILER /usr/local/bin/clang++-wasi-threads-sysroot.sh)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /wasi-runtimes/wasmtime/bin/wasmtime-pwd-threads.sh)
