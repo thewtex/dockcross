83766c341d4fec99b3d0e67aad5d38d3cd9c1cad
Jonas Vautherin <dev@jonas.vautherin.ch>
2025-03-08 16:54:57 +0100

    Add CMAKE_PREFIX_PATH to CMAKE_FIND_ROOT_PATH

--- linux-armv6/Toolchain.cmake
+++ linux-armv6/Toolchain.cmake
@@ -10,7 +10,7 @@ set(CMAKE_Fortran_COMPILER $ENV{FC})
 
 set(CMAKE_CXX_FLAGS "-I ${cross_root}/include/")
 
-set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
+list(APPEND CMAKE_FIND_ROOT_PATH ${CMAKE_PREFIX_PATH} ${cross_root} ${cross_root}/${cross_triple})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
